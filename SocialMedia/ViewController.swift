da436730ae6c79b6762dc159804f351a4cda9189
AdamDono <111118088+AdamDono@users.noreply.github.com>
2023-07-12 11:48:26 +0200

    Initial Commit

---
+++ SocialMedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialMedia
+//
+//  Created by DA M1 MAC 89 on 2023/07/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
